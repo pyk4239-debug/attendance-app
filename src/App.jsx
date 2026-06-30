3e4a26f25bf14877202e3209bc383eb0e4ab6adb
pyk4239-debug <pyk4239@gmail.com>
2026-06-30 10:17:59 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6175,7 +6175,7 @@ function InsuranceSection({ users, memberInfo, onBack }) {
           </div>
           {/* 하단 엑셀 버튼 */}
           <button onClick={downloadExcel}
-            style={{ width: "100%", marginTop: 16, padding: "14px 0", borderRadius: 14, border: "none", background: "#0369a1", color: "#fff", fontSize: 15, fontWeight: 800, cursor: "pointer" }}>
+            style={{ width: "100%", marginTop: 16, marginBottom: 80, padding: "14px 0", borderRadius: 14, border: "none", background: "#0369a1", color: "#fff", fontSize: 15, fontWeight: 800, cursor: "pointer" }}>
             📥 엑셀 다운로드
           </button>
         </>)}
